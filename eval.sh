15de24d96ee98b83ceb5c16317ead72f8b75a832
zhangxz <zhangxz18@mails.tsinghua.edu.cn>
2022-12-22 19:19:45 +0800

    change palette for ade20k

--- eval.sh
+++ eval.sh
@@ -4,7 +4,7 @@ export CUDA_VISIBLE_DEVICES=3
 # python evaluate.py --data-dir ../datasets/cityscape  --recurrence 2 --model ccnet --restore-from ./snapshots/CS_scenes_15000ccnet.pkl --whole True --gpu 0 --batch-size 1 >../log/eval_resnet_CS.log
 
 # Resnet + CCNet + ADE20K
-python evaluate.py --num-classes 150 --datasets ade --data-dir ../datasets/ADEChallengeData2016 --data-list ./dataset/list/ade20k/ade20k_val.txt --recurrence 2 --model ccnet --restore-from ./snapshots/ADE20k_15000ccnet.pkl --gpu 0 --batch-size 1 >../log/eval_resnet_ADE_notcrop_randomsize5_batch4_15k.log
+python evaluate.py --num-classes 150 --datasets ade --data-dir ../datasets/ADEChallengeData2016 --data-list ./dataset/list/ade20k/ade20k_val.txt --recurrence 2 --model ccnet --restore-from ./snapshots/ADE20k_15000_ccnet.pkl --gpu 0 --batch-size 1 >../log/eval_just_for_picture.log
 
 # VAN + CCNet + Cityscape
 # python evaluate.py --data-dir ../datasets/cityscape  --recurrence 2 --model van --restore-from ./snapshots/CS_scenes_15000_van.pkl --whole True --gpu 4 --batch-size 1 >../log/eval_van_CS_b4_1w5.log
